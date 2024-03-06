aac1fa968a8c9810b59c2cf924db0a676cca83a2
Nikki2456 <129265960+Nikki2456@users.noreply.github.com>
2024-03-06 11:02:01 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,13 +2,17 @@ const accountId =144553 //const is keyword
 let accountEmail = "Maarnanikhil@gmail.com"
 var accountPassword = "12345"
 accountCity = "Hyderabad"
+let accountState;
 
 //01_basics/01_variables.js
 //accountId = 21
-
-
+//{} are scope
+/*
+prefer not use var 
+because of issue in block scope and functional scope
+*/
 accountEmail = "nikki@gmail.com"
 accountPassword = "212121"
 accountCity = "telangana"
 console.log(accountId);
-console.table[accountEmail, accountPassword, accountCity]
+console.table([accountEmail, accountPassword, accountCity])
