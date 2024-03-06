1a4cec57f7b1a23bde680cdd4050ff8befe3bd6f
Nikki2456 <129265960+Nikki2456@users.noreply.github.com>
2024-03-06 10:26:53 +0530

    Create 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId =144553 //const is keyword
+let accountEmail = "Maarnanikhil@gmail.com"
+var accountPassword = "12345"
+accountCity = "Hyderabad"
+
+//accountId = 21
+
+console.let(accountId);
+console.log(accountEmail);
