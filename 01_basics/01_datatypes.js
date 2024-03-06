0ec3c3a00f9061a6c87bd1ce72f77b0900133510
Nikki2456 <129265960+Nikki2456@users.noreply.github.com>
2024-03-06 11:45:34 +0530

    Update 01_datatypes.js

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as new version
+
+//alert( ) we are using node js not browser
+
+console.log(3 + 3)// code readability should high
+//https: tc: 39
+//mdn web log
+
+let name = "nikhil"
+let age = 18
+let isLoggedIn = false
+let state;
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean => true/false
+//null =. standalone value "is empty" 
+//symbol => unique
+// undefined =>
+
+//object
+
+console.log(typeof "nikhil");
