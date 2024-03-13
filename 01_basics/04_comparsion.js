43d4f0f1979a4e002924216b7c6c59f27d3dc23d
Nikki2456 <129265960+Nikki2456@users.noreply.github.com>
2024-03-13 11:32:02 +0530

    Update 04_comparsion.js

--- 01_basics/04_comparsion.js
+++ 01_basics/04_comparsion.js
@@ -1,6 +1,18 @@
 /*console.log(2>1);
 console.log(2>=);
-*/
+
+basic comparsion
 
 console.log("2">1);
 console.log('02'>1);
+
+console.log(null >0);
+console.log(null==0);
+console.log(null>=0);
+
+*/
+console.log(undefined ==0);
+//compare and equal check is different from each other.
+//===strip check, it also value and data type
+
+console.log("2"===2);
